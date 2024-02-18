59f0fb6198f92e088588b59d1f99c904c27942f1
Galina684 <galina684@gmail.com>
2024-02-18 12:03:54 +0300

    start

---
+++ src/main/java/pro/sky/schedulefortelegrambot/ScheduleForTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.schedulefortelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleForTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleForTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/schedulefortelegrambot/ScheduleForTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.schedulefortelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleForTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
